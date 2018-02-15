aea76d3e19e00f4fa139f067a22392d8979826fd
cotestatnt <cotestatnt@yahoo.com>
2018-02-15 10:26:19 +0100

    Add files via upload

---
+++ ATmegaNode2/Debug/ATmegaNode2.cpp.d
@@ -0,0 +1,25 @@
+C:\Users\MAES\AppData\Local\Temp\VMBuilds\ATmegaNode2\pro_16MHzatmega328\Debug\ATmegaNode2.cpp.o: \
+ C:\Users\MAES\AppData\Local\Temp\VMBuilds\ATmegaNode2\pro_16MHzatmega328\Debug\ATmegaNode2.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h \
+ C:\Users\MAES\Documents\Arduino\libraries\RF24Network/RF24Network.h \
+ C:\Users\MAES\Documents\Arduino\libraries\RF24Network/RF24Network_config.h \
+ C:\Users\MAES\Documents\Arduino\libraries\RF24/RF24_config.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ C:\Users\MAES\Documents\Arduino\libraries\RF24/RF24.h \
+ C:\Users\MAES\Documents\Arduino\libraries\RF24/RF24_config.h \
+ C:\Users\MAES\Documents\Arduino\libraries\CryptoLight/CBC.h \
+ C:\Users\MAES\Documents\Arduino\libraries\CryptoLight/Cipher.h \
+ C:\Users\MAES\Documents\Arduino\libraries\CryptoLight/BlockCipher.h \
+ C:\Users\MAES\Documents\Arduino\libraries\CryptoLight/AES.h \
+ C:\Users\MAES\AppData\Local\Temp\VMBuilds\ATmegaNode2\pro_16MHzatmega328\Debug\debug.h
